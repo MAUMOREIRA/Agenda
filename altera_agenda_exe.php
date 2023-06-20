36d09ebef78d61ed404fb7f6cf0ec0ec7fceaee6
MAU MOREIRA <124677551+MAUMOREIRA@users.noreply.github.com>
2023-06-20 20:21:00 -0300

    Pronto

--- altera_agenda_exe.php
+++ altera_agenda_exe.php
@@ -1,4 +1,5 @@
-<?php  
+<?php
+
     include('conexao.php');
     $id_agenda = $_POST['id_agenda'];
     $nome = $_POST['nome'];
@@ -9,28 +10,31 @@
     $estado = $_POST['estado'];
     $telefone = $_POST['telefone'];
     $celular = $_POST['celular'];
-    $email= $_POST['email'];
+    $email = $_POST['email'];
 
     echo "<h1>Alteração de dados </h1>";
-    echo "<p>Agenda: $nome</p>";
-    $sql = "UPDATE agenda SET
-            nome='$nome',
-            apelido='$apelido',
-            endereco='$endereco',
-            bairro='$bairro',
-            cidade='$cidade',
-            estado='$estado',
-            telefone='$telefone',
-            celular='$celular',
-            email='$email'
-            WHERE id_agenda=$id_agenda";
-        
-    //echo $sql. "<br>";
+    echo "<p> Usuário: $nome </p>";
+
+    $sql = "UPDATE agenda SET 
+        nome = '$nome',
+        apelido = '$apelido',
+        endereco = '$endereco',
+        bairro = '$bairro',
+        cidade = '$cidade',
+        estado = '$estado',
+        telefone = '$telefone',
+        celular = '$celular',
+        email = '$email'
+        WHERE id_agenda = $id_agenda";
+
+    echo $sql. "<br>";
     $result = mysqli_query($con, $sql);
-     if($result)
-        echo "Dados alterados com sucesso!<br>";
-    else
-        echo "Erro ao alterar dados: ". mysqli_error($con)."!";
-  
+
+    if($result)
+        echo "Dados alterados com sucesso! <br>";
+
+    else 
+        echo "Erro ao alterar dados: ".mysqli_error($con)."!";
+
 ?>
 <a href="index.php">Voltar</a>
